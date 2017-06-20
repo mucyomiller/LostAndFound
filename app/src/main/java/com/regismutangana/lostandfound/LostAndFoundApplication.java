bb189f5c6c13f509aa908ea02463300cb38bf0ff
Mucyo F Miller <mucyomiller@gmail.com>
2017-06-20 11:44:29 +0200

    add multidex support

---
+++ app/src/main/java/com/regismutangana/lostandfound/LostAndFoundApplication.java
@@ -0,0 +1,23 @@
+package com.regismutangana.lostandfound;
+
+import android.app.Application;
+import android.content.Context;
+import android.support.multidex.MultiDex;
+import android.support.multidex.MultiDexApplication;
+
+/**
+ * Created by miller on 6/20/17.
+ */
+
+public class LostAndFoundApplication extends MultiDexApplication {
+    @Override
+    protected void attachBaseContext(Context context) {
+        super.attachBaseContext(context);
+        MultiDex.install(this);
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
