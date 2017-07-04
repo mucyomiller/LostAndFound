9671872d193d3592a6857386ecf6b4ba47a82c9b
Mucyo F Miller <mucyomiller@gmail.com>
2017-07-04 10:58:27 +0200

    add viewpager on Found Activity

--- app/src/main/java/com/regismutangana/lostandfound/Found/FoundActivity.java
+++ app/src/main/java/com/regismutangana/lostandfound/Found/FoundActivity.java
@@ -2,13 +2,15 @@ package com.regismutangana.lostandfound.Found;
 
 import android.content.Context;
 import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-
 import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
+import com.regismutangana.lostandfound.Home.SectionsPagerAdapter;
 import com.regismutangana.lostandfound.R;
 import com.regismutangana.lostandfound.Utils.BottomNavigationViewHelper;
 
@@ -22,6 +24,21 @@ public class FoundActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_found);
         setupBottomNavigationView();
+        setupViewPager();
+    }
+
+    private void setupViewPager(){
+        SectionsPagerAdapter adapter = new SectionsPagerAdapter(getSupportFragmentManager());
+        adapter.addFragemnt(new FoundFragmentCards());
+        adapter.addFragemnt(new FoundFragmentAutomobile());
+        adapter.addFragemnt(new FoundFragmentGadgets());
+        ViewPager viewPager = (ViewPager)findViewById(R.id.container);
+        viewPager.setAdapter(adapter);
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
+        tabLayout.getTabAt(0).setIcon(R.drawable.ic_cards);
+        tabLayout.getTabAt(1).setIcon(R.drawable.ic_cars);
+        tabLayout.getTabAt(2).setIcon(R.drawable.ic_gadgets);
     }
 
     /**
