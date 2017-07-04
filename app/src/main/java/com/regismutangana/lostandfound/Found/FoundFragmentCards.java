5a8d26f3d82833853541274bda0d94e27d56dd02
Mucyo F Miller <mucyomiller@gmail.com>
2017-07-04 13:51:23 +0200

    add ugly cardView :)

--- app/src/main/java/com/regismutangana/lostandfound/Found/FoundFragmentCards.java
+++ app/src/main/java/com/regismutangana/lostandfound/Found/FoundFragmentCards.java
@@ -3,13 +3,19 @@ package com.regismutangana.lostandfound.Found;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.regismutangana.lostandfound.Model.Card;
 import com.regismutangana.lostandfound.R;
+import com.regismutangana.lostandfound.ViewHolder.CardsHolder;
 
 /**
  * Created by miller on 6/18/17.
@@ -21,16 +27,39 @@ public class FoundFragmentCards extends Fragment {
     private FirebaseAuth mAuth;
     private DatabaseReference mFirebaseDbRef;
     private FirebaseDatabase mFirebaseInstance;
+    private FirebaseRecyclerAdapter mAdapter;
 
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view =inflater.inflate(R.layout.fragment_found_cards,container,false);
+        RecyclerView foundcardsrec = (RecyclerView) view.findViewById(R.id.foundcardsrec);
+        foundcardsrec.setLayoutManager(new LinearLayoutManager(getContext()));
         //initialize_auth
         mAuth = FirebaseAuth.getInstance();
         //initialize firebase
         mFirebaseInstance = FirebaseDatabase.getInstance();
+        mFirebaseDbRef = mFirebaseInstance.getReference("cards").child("lost");
+        mAdapter = new FirebaseRecyclerAdapter<Card, CardsHolder>(
+                Card.class,
+                R.layout.card_item,
+                CardsHolder.class,
+                mFirebaseDbRef) {
+            @Override
+            public void populateViewHolder(CardsHolder holder, Card card, int position) {
+                holder.setIdnumber(card.getIdNumber());
+                holder.setOwner(card.getOwnerName());
+                holder.setLocation(card.getLostLocation());
+            }
+        };
+        foundcardsrec.setAdapter(mAdapter);
         return view;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        mAdapter.cleanup();
     }
 }
--- app/src/main/java/com/regismutangana/lostandfound/ViewHolder/CardsHolder.java
+++ app/src/main/java/com/regismutangana/lostandfound/ViewHolder/CardsHolder.java
@@ -1,8 +1,34 @@
 package com.regismutangana.lostandfound.ViewHolder;
 
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.TextView;
+
+import com.regismutangana.lostandfound.R;
+
 /**
  * Created by miller on 7/4/17.
  */
 
-public class CardsHolder {
+public class CardsHolder extends RecyclerView.ViewHolder {
+    private final TextView card_idnumber;
+    private final TextView card_owner;
+    private final TextView card_location;
+
+    public CardsHolder(View itemView) {
+        super(itemView);
+        card_idnumber = (TextView) itemView.findViewById(R.id.card_idnumber);
+        card_owner = (TextView) itemView.findViewById(R.id.card_owner);
+        card_location = (TextView) itemView.findViewById(R.id.card_location);
+    }
+    public void setIdnumber(String name) {
+        card_idnumber.setText(name);
+    }
+
+    public void setOwner(String message) {
+        card_owner.setText(message);
+    }
+    public void setLocation(String message) {
+        card_location.setText(message);
+    }
 }
